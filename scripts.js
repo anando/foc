c9d07d30c75bdde7d76a6ed1e6e67f81f1bf34a2
anando <anandaggarwal@outlook.com>
2015-07-27 15:17:08 +0100

    d

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-$.getJSON('http://api.tumblr.com/v2/tagged', {tag: 'gif', api_key: 'fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4'}, function(data) {
+$.getJSON('https://api.tumblr.com/v2/tagged', {tag: 'gif', api_key: 'fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4'}, function(data) {
 	console.log(data);
 });
